34f4489eb79cc372c5d4681cd1665e1d3f9ea8eb
yutanakano <yutanakano395@icloud.com>
2018-12-07 09:04:10 +0900

    Add KamizaComponent

---
+++ src/components/Kamiza.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <link
+      rel="stylesheet"
+      href="https://cdn.materialdesignicons.com/2.5.94/css/materialdesignicons.min.css"
+    />
+    <link
+      href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet"
+    />
+    <v-toolbar color="cyan" dark>
+      <kondate></kondate>
+
+      <v-toolbar-title>{{ title }}</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <kenmi></kenmi>
+
+      <zendate></zendate>
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+import "../plugins/vuetify";
+import Kondate from "./Kondate";
+import Zendate from "./Zendate";
+import Kenmi from "./Kenmi";
+
+export default {
+  components: { Kenmi, Zendate, Kondate },
+  props: {
+    title: {
+      type: String,
+      default: "タイトル"
+    }
+  }
+};
+</script>
---
+++ src/stories/kamiza.stories.js
@@ -0,0 +1,29 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { storiesOf } from "@storybook/vue";
+
+import VueInfoAddon from "storybook-addon-vue-info";
+import { withMarkdownNotes } from "@storybook/addon-notes";
+import { withKnobs, text } from "@storybook/addon-knobs";
+
+import Kamiza from "../components/Kamiza.vue";
+
+storiesOf("Kamiza", module)
+  .addDecorator(VueInfoAddon)
+  .addDecorator(withKnobs)
+  .add(
+    "with header",
+    withMarkdownNotes(`
+# Title
+  - Knobsの用途
+      - headerに表示させたいタイトルを入力してください
+    `)(() => {
+      const Title = text("title", "タイトル");
+      return {
+        components: { Kamiza },
+        template: `<kamiza title="${Title}"/>`,
+        propsDescription: {
+          title: "ヘッダーに表示させるタイトル"
+        }
+      };
+    })
+  );
