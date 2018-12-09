bc92f271f6654f0daf8465ddd41f2d744c34e902
yutanakano <yutanakano395@icloud.com>
2018-12-10 02:29:15 +0900

    Add DaimeiComponent

---
+++ src/components/Daimei.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <link
+      rel="stylesheet"
+      href="https://cdn.materialdesignicons.com/2.5.94/css/materialdesignicons.min.css"
+    />
+    <link
+      href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet"
+    />
+    <v-toolbar-title>{{ title }}</v-toolbar-title>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: "タイトル"
+    }
+  }
+};
+</script>
