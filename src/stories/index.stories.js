26854815996410b4225b52152af772bbb9caa438
yutanakano <yutanakano395@icloud.com>
2018-12-11 10:40:29 +0900

    Execute lint

--- src/stories/index.stories.js
+++ src/stories/index.stories.js
@@ -3,34 +3,43 @@ import { storiesOf } from "@storybook/vue";
 import { action } from "@storybook/addon-actions";
 import { linkTo } from "@storybook/addon-links";
 
-import VueInfoAddon from 'storybook-addon-vue-info';
-import { withMarkdownNotes } from '@storybook/addon-notes'
+import VueInfoAddon from "storybook-addon-vue-info";
+import { withMarkdownNotes } from "@storybook/addon-notes";
 
 import MyButton from "../components/MyButton.vue";
 
 storiesOf("Button", module)
   .addDecorator(VueInfoAddon)
-  .add('with text', withMarkdownNotes(`
+  .add(
+    "with text",
+    withMarkdownNotes(`
   # 用途
   - なし
     `)(() => ({
       components: { MyButton },
       template: '<my-button @click="action">Hello Button</my-button>',
       methods: { action: action("clicked") }
-    })))
-  .add("with JSX", withMarkdownNotes(`
+    }))
+  )
+  .add(
+    "with JSX",
+    withMarkdownNotes(`
   # 用途
   - なし
     `)(() => ({
-    components: { MyButton },
-    template: '<my-button @click="action">With JSX</my-button>',
-    methods: { action: linkTo("Button", "with some emoji") }
-  })))
-  .add("with some emoji", withMarkdownNotes(`
+      components: { MyButton },
+      template: '<my-button @click="action">With JSX</my-button>',
+      methods: { action: linkTo("Button", "with some emoji") }
+    }))
+  )
+  .add(
+    "with some emoji",
+    withMarkdownNotes(`
   # 用途
   - なし
     `)(() => ({
-    components: { MyButton },
-    template: '<my-button @click="action">😀 😎 👍 💯</my-button>',
-    methods: { action: action("clicked") }
-  })));
+      components: { MyButton },
+      template: '<my-button @click="action">😀 😎 👍 💯</my-button>',
+      methods: { action: action("clicked") }
+    }))
+  );
